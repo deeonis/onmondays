aa292a70300f6f4e1581f5527129df48e7b5e49a
Denis Kuznetsov <denis@Deniss-MacBook-Pro.local>
2025-01-23 00:46:03 +0300

    fix title and desc

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,8 +1,8 @@
 export const AppConfig = {
-	site_name: 'AstroPress',
-	title: 'AstroPress | Astro and Tailwind CSS',
-	description: 'Boilerplate built with Astro and Tailwind CSS',
-	author: 'Nicolas',
-	locale_region: 'de-en',
-	locale: 'en'
+	site_name: 'Denis Kuznetsov',
+	title: 'Denis Kuznetsov',
+	description: 'Denis Kuznetsov personal site',
+	author: 'Denis',
+	locale_region: 'en-ru',
+	locale: 'ru'
 };
